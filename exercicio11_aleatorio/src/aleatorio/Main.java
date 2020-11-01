c4ad50d190b93af49082fbbab68daf9291f1371f
dcancelas <dcancelasgarcia@danielcastelao.org>
2020-11-01 18:11:09 +0100

    Added new exercise and some changes

---
+++ exercicio11_aleatorio/src/aleatorio/Main.java
@@ -0,0 +1,11 @@
+package aleatorio;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File(System.getProperty("user.dir")+"/exercicio11_aleatorio/src", "produtos.txt");
+
+    }
+}
